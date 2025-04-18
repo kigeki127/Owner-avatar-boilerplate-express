a3760e1938a3c78a41f500d09de2be68c77517fe
egemen <egemeno@globasteer.com>
2025-04-18 18:12:33 +0300

    bugFix: JSON bug

--- myApp.js
+++ myApp.js
@@ -1,17 +1,17 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World");
+//console.log("Hello World");
 
 //app.get("/", function(req, res) {
 //  res.send("Hello Express");
 //});
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/views/index.html");
-});
+//app.get("/", function(req, res) {
+//  res.sendFile(__dirname + "/views/index.html");
+//});
 
-app.use("/public", express.static(__dirname + "/public"));
+//app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
   res.json({
