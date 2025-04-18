dad51979cb4140819b9e069d4bc2ce6e993b052c
egemen <egemeno@globasteer.com>
2025-04-18 16:49:16 +0300

    bugFix: assest path corrected

--- myApp.js
+++ myApp.js
@@ -1,4 +1,4 @@
-let express = require('express');
+\let express = require('express');
 let app = express();
 
 console.log("Hello World");
@@ -11,7 +11,7 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.use("/public/style.css", express.static(__dirname + "/views/index.html"));
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
