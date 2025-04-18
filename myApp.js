b677a68be2bb76bca4d5f012558b59bb63290c64
egemen <egemeno@globasteer.com>
2025-04-18 12:01:21 +0300

    bugFix: Send File code has been corrected

--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ app.get("/", function(req, res) {
 });
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 
