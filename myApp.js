a131541cfd4b29a57365e2d9e34e3cccc609435b
egemen <egemeno@globasteer.com>
2025-04-18 12:15:08 +0300

    bugFix: Send File code has been corrected

--- myApp.js
+++ myApp.js
@@ -3,9 +3,9 @@ let app = express();
 
 console.log("Hello World");
 
-app.get("/", function(req, res) {
-  res.send("Hello Express");
-});
+//app.get("/", function(req, res) {
+//  res.send("Hello Express");
+//});
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
